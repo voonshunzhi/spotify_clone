820c9c8e49f33225734cce504693317565627f6c
Voon <voonshunzhi@gmail.com>
2018-04-30 17:52:10 +0800

    Done

--- Spotify_extra_info.php
+++ Spotify_extra_info.php
@@ -16,8 +16,34 @@ In simple words, CLASS are files that store variable and fucntion so that you ca
 
 Log in info :
 - How do we make sure that only logged in user can access the content of our webpages?
-- So , We will have to remember whether they have logged in or not
+- So , We will have to remember whether they have logged in or not using session superglobals
 
 
+CSS length unit :
+- vh
+Equal to 1% of the height of the viewport's initial containing block.
+-- EG : if 100vh, it will cover 100% of viewport height
+- vw
+Equal to 1% of the width of the viewport's initial containing block.
 
+*** Imagin margin as BLANK(NO TEXT,NO CONTENT)SPACE OUTSIDE THE ELEMENT, this will clarify a lot of things.
+
+NOTE THAT : The top and bottom margins have NO EFFECT on non-replaced INLINE ELEMENTS, such as <span> or <code>.
+
+CSS position property :
+- fixed : relative to the viewport blocks directlu
+- relative : relative to its original position
+- absolute : relative to it containing block (closest positioned ancestor) which has a position property as well
+- These property are used with top, right, bottom,left
+
+Caching problem with CSS : Causes CSS not updating
+- Caching is a technique that stores a copy of a given resource and serves it back when requested. When a web cache has a requested resource in its store, it intercepts the request and returns its copy instead of re-downloading from the originating server. This achieves several goals: it eases the load of the server that doesn’t need to serve all clients itself, and it improves performance by being closer to the client, i.e., it takes less time to transmit the resource back. For a web site, it is a major component in achieving high performance. On the other side, it has to be configured properly as not all resources stay identical forever: it is important to cache a resource only until it changes, not longer.
+
+- There are several types of cache :
+1) No cache :
+- All identical request are going through the server
+2) Local cache :
+- The 1st request is going throught the server but the subsequent identical request are not even sent but served by local web browser cache.
+3) Shared cache :
+- 1st request is going through the server, subsequent request are served by a common cache of a web browser.
 -->
--- include/index.php
+++ include/index.php
@@ -18,75 +18,104 @@
 <head>
     <meta charset="UTF-8">
     <title>Welcome to Spotify!</title>
+    <link rel="stylesheet" href="../../CSS_WEB_DESIGN/normalize.css">
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
-    
-    <div id="nowPlayingBarContainer">
-        <div id="nowPlayingBar">
-            <div id="nowPlayingLeft">
-                <div class="content">
-<!-- span tags in html : much like div but it is inline element rather than block elements, it doesn't really represent something but it is used to group elements like for styling purpose-->
-                    <span class="albumLink">
-                        <img src="../assets/img/square_pic.jpg" class="albumArtwork">
-                    </span>
-                    <div class="trackInfo">
-                        <span class="trackName">
-                            <span>Best Song Ever</span>
+    <div id="mainContainer">
+           <div id="topContainer">
+                <div id="navBarContainer">
+                    <nav class="navBar">
+                        <a href="index.php">
+                            <img src="../assets/img/icons/dog_logo%20(2).png" alt="logo"  class="logo">
+                        </a>
+                        <div class="group">
+                            <div class="navItem">
+                                <a href="search.php" class="navItemLink">Search
+                                <img src="../assets/img/icons/search.png" alt="Search" class="Search"></a>
+                            </div>
+                        </div>
+                        <div class="group">
+                            <div class="navItem">
+                                <a href="browse.php" class="navItemLink">Browse</a>
+                            </div>
+                            <div class="navItem">
+                                <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                            </div>
+                            <div class="navItem">
+                                <a href="profile.php" class="navItemLink">Reece Kenney</a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+           </div>
+            <div id="nowPlayingBarContainer">
+            <div id="nowPlayingBar">
+                <div id="nowPlayingLeft">
+                    <div class="content">
+    <!-- span tags in html : much like div but it is inline element rather than block elements, it doesn't really represent something but it is used to group elements like for styling purpose-->
+                        <span class="albumLink">
+                            <img src="../assets/img/square_pic.jpg" class="albumArtwork">
                         </span>
-                        <span class="artistName">
-                            <span>One Direction</span>
-                        </span>
+                        <div class="trackInfo">
+                            <span class="trackName">
+                                <span>Best Song Ever</span>
+                            </span>
+                            <span class="artistName">
+                                <span>One Direction</span>
+                            </span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div id="nowPlayingCenter">
-                <div class="content playerControls">
-                    <div class="buttons">
-                        <button class="controlButton shuffle" title="Shuffle Button">
-                        <img src="../assets/img/icons/shuffle.png" alt="Shuffle">
-                        </button>
-                         <button class="controlButton previous" title="Previous Button">
-                        <img src="../assets/img/icons/previous.png" alt="previous">
-                        </button>
-                        <button class="controlButton pause" title="Pause Button" style="display:none;">
-                        <img src="../assets/img/icons/pause.png" alt="pause" style="height:32px;width:32px">
-                        </button>
-                         <button class="controlButton play" title="Play Button">
-                        <img src="../assets/img/icons/play.png" alt="play" style="height:32px;width:32px">
-                        </button>
-                         <button class="controlButton next" title="Next Button">
-                        <img src="../assets/img/icons/next.png" alt="next">
-                        </button>
-                         <button class="controlButton repeat" title="Repeat Button">
-                        <img src="../assets/img/icons/repeat.png" alt="repeat">
-                        </button>
+                <div id="nowPlayingCenter">
+                    <div class="content playerControls">
+                        <div class="buttons">
+                            <button class="controlButton shuffle" title="Shuffle Button">
+                            <img src="../assets/img/icons/shuffle.png" alt="Shuffle">
+                            </button>
+                             <button class="controlButton previous" title="Previous Button">
+                            <img src="../assets/img/icons/previous.png" alt="previous">
+                            </button>
+                            <button class="controlButton pause" title="Pause Button" style="display:none;">
+                            <img src="../assets/img/icons/pause.png" alt="pause" style="height:32px;width:32px">
+                            </button>
+                             <button class="controlButton play" title="Play Button">
+                            <img src="../assets/img/icons/play.png" alt="play" style="height:32px;width:32px">
+                            </button>
+                             <button class="controlButton next" title="Next Button">
+                            <img src="../assets/img/icons/next.png" alt="next">
+                            </button>
+                             <button class="controlButton repeat" title="Repeat Button">
+                            <img src="../assets/img/icons/repeat.png" alt="repeat">
+                            </button>
+                        </div>
+                        <div class="playBackBar">
+                            <span class="progressTime current">0.00</span>
+                            <div class="progressBar">
+                                <div class="progressBarBg">
+                                    <div class="progress"></div>
+                                </div>
+                            </div>
+                            <span class="progressTime remaining">0.00</span>
+                        </div>
                     </div>
-                    <div class="playBackBar">
-                        <span class="progressTime current">0.00</span>
+                </div>
+                <div id="nowPlayingRight">
+                    <div class="volumeBar">
+                        <button class="controlButton volume" title="Volume button" >
+                            <img src="../assets/img/icons/volume.png" alt="Volume" style="width:25px;cursor:pointer;">
+                        </button>
                         <div class="progressBar">
                             <div class="progressBarBg">
                                 <div class="progress"></div>
                             </div>
-                        </div>
-                        <span class="progressTime remaining">0.00</span>
-                    </div>
-                </div>
-            </div>
-            <div id="nowPlayingRight">
-                <div class="volumeBar">
-                    <button class="controlButton volume" title="Volume button" >
-                        <img src="../assets/img/icons/volume.png" alt="Volume" style="width:25px;cursor:pointer;">
-                    </button>
-                    <div class="progressBar">
-                        <div class="progressBarBg">
-                            <div class="progress"></div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    
     
     
 </body>
